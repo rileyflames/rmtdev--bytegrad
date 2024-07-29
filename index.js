04e5dbdcfcc35c1b3e2fb34f457f982b569b8d04
rileyflames <Rileyflamez@proton.me>
2024-07-29 10:37:25 +0200

    added new files to the project
    Added 3 files to the main folder which are the main files I will be working with at the beginning.

---
+++ index.js
@@ -0,0 +1,22 @@
+// global selectors
+const bookmarksBtnEl = document.querySelector('.bookmarks-btn');
+const errorEl = document.querySelector('.error');
+const errorTextEl = document.querySelector('.error__text');
+const jobDetailsEl = document.querySelector('.job-details');
+const jobDetailsContentEl = document.querySelector(".job-details__content");
+const jobListBookmarksEl = document.querySelector('.job-list--bookmarks');
+const jobListSearchEl = document.querySelector(".job-list--search");
+const numberEl = document.querySelector(".count__number");
+const paginationEl = document.querySelector(".pagination");
+const paginationBtnNextEl = document.querySelector(".pagination__button--next");
+const paginationBtnBackEl = document.querySelector(".pagination__button--back");
+const paginationNumberNextEl = document.querySelector(".pagination__number--next");
+const paginationNumberBackEl = document.querySelector(".pagination__number--back");
+const searchFormEl = document.querySelector(".search");
+const searchInputEl = document.querySelector(".search__input");
+const sortingEl = document.querySelector(".sorting");
+const sortingBtnRelevantEl = document.querySelector(".sorting__button--relevant");
+const sortingBtnRecentEl = document.querySelector(".sorting__button--recent");
+const spinnerSearchEl = document.querySelector(".spinner--search");
+const spinnerJobDetailsEl = document.querySelector(".spinner--job-details");
+// --search component
