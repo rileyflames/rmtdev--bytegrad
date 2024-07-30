0c657af3510d85ba7ea8f1874da4d1b7e47646b0
rileyflames <Rileyflamez@proton.me>
2024-07-30 15:26:34 +0200

    added a new jobDetails.js file to contain the job details and add more modularity

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,6 +8,7 @@ import {
 } from '../common.js';
 import renderError from './Error.js';
 import renderSpinner from './Spinner.js';
+import renderJobList from './JobList.js';
 
 const submitHandler = (event) =>{
     //prevent default behavior 
@@ -50,29 +51,8 @@ const submitHandler = (event) =>{
         numberEl.textContent = jobItems.length;
 
         // render job items in search job list
-        jobItems.slice(0,7).forEach(jobItem => {
-
-            const newJobItemHTML =`
-                <li class="job-item">
-                    <a class="job-item__link" href="${jobItem.id}">
-                        <div class="job-item__badge">${jobItem.badgeLetters}</div>
-                        <div class="job-item__middle">
-                            <h3 class="third-heading">${jobItem.title}</h3>
-                            <p class="job-item__company">${jobItem.company}</p>
-                            <div class="job-item__extras">
-                                <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i> ${jobItem.duration}</p>
-                                <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i> ${jobItem.salary}</p>
-                                <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i> ${jobItem.location}</p>
-                            </div>
-                        </div>
-                        <div class="job-item__right">
-                            <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
-                            <time class="job-item__time">${jobItem.daysAgo}d</time>
-                        </div>
-                    </a>
-                </li>`;
-                jobListSearchEl.insertAdjacentHTML('beforeend', newJobItemHTML);
-        });
+        renderJobList(jobItems);
+        
     })
     .catch(error => console.log(error));
 }
